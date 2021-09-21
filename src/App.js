c07411d5135fb5dd5619d643242f540c3d9dea52
EmilyDSarani <emily.sarani@gmail.com>
2021-09-21 17:27:57 -0500

    almost working!!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
+import React, { Component } from 'react';
 import Header from './components/Header';
-import './App.css';
+import ImageList from './components/ImageList'
+import dataCreatures from './data.js'
+import './App.css'
+//will need to import the imagelist over here, this is where I will use that list to map out the items 
+//will also do the filter, maps, section, option, handlechange
+//e.target.keyword
 
-//need to import header and imageitem. This is gonna look clean. 
-function App() {
-  return (
-    <>
-    <Header />
 
-    </>
-  );
+
+export default class ImageItem extends Component {
+
+state = {
+    keyword: ''
 }
 
-export default App;
+handleChange = (e) => {
+    this.setState({name: e.target.keyword})
+}
+
+    render()
+  
+     {
+        const whackCreatures = dataCreatures
+        .filter (creature => {
+            if(!this.state.keyword){
+                return true
+            }
+            return creature.keyword === this.state.keyword
+        })
+
+        return (
+            <div className='creatures'>
+                <Header />
+
+
+                <p>show all {this.state.keyword} creatures with horns</p>
+
+                <select onChange = {this.handleChange}>
+                    <option value = ''>All Creatures </option>
+                    <option value = 'unicorns'>United Unicorn</option>
+                    <option value = 'rhino'> Rockin Rhinos</option>
+                    <option value = 'narhwals'>Narwhals Cause Commotions</option>
+                    <option value = 'triceratops'>TriCERAtops</option>
+                    <option value = 'markhor'>Mighty Morphin Markhors</option>
+                    <option value = 'mouflon'>I like to Mouflon Mouflon</option>
+                    <option value = 'chameleon'>Chamillionaire Chameleons</option>
+                    <option value = 'lizard'>Horned Lizard, some call it a toad</option>
+                    <option value = 'dragon'>I am Fire. I am Death.</option>
+                </select>
+
+                {
+                whackCreatures.map(creature => <ImageList
+            url = {creature.url}
+            title = {creature.title}
+            description = {creature.description}
+            keyword = {creature.keyword}
+            horns = {creature.horns}    
+            />)
+                }
+            </div>
+        )
+    }
+}
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react'
+
 //I am going to assume I would assume this is where I need to set up the render. like, the whole {this.prop.img} {this.prop.name} {this.prop.information} {this.prop.key} {this.prop.horn} 
 export default class ImageList extends Component {
     render() {
         return (
-            <div>
-                <ul>
-                    <li>
-                        {this.props.img}
-                        A {this.prop.name} 
-                        {this.prop.information} 
-                        {this.prop.key} 
-                        it has {this.prop.horn} 
-
-                    </li>
-                </ul>
+            <div className="images">
+            <img className= 'img' src={this.props.url} alt='urls'/>
+            {this.props.title} 
+            {this.props.description} 
+            {this.props.keyword} 
+            {this.props.horns} 
             </div>
+            
         )
     }
 }

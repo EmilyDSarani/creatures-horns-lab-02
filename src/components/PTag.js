5c08f8da89eff2a9d917ce4304c56976652d9bea
EmilyDSarani <emily.sarani@gmail.com>
2021-09-22 17:07:28 -0500

    it says i needed to comment

--- src/components/PTag.js
+++ src/components/PTag.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+//in this page, it is just an easy...callback? is that the word here?
+//you are taking the state on the app.js page then setting this to a prop so that as state changes the words/numbers will appear?
 export default class PTag extends Component {
     render() {
         return (
